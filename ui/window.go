4ab36cd4cb6a8642995ad7ba7e1eebf7b021c4c4
OlegKorniychuk <kornoleg576@gmail.com>
2023-04-26 20:04:40 +0300

    Now certainly fixed default bg color

--- ui/window.go
+++ ui/window.go
@@ -133,7 +133,7 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
 	pw.w.Fill(image.Rect(250, 300, 550, 400), color.RGBA{R: 255, G: 255, B: 0, A: 1}, draw.Src)
