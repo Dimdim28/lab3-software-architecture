6f7b25e84ad3c7ce0c8082b576126343b9ccb96f
OlegKorniychuk <kornoleg576@gmail.com>
2023-04-28 22:23:39 +0300

    Finally implemented command parser

--- ui/window.go
+++ ui/window.go
@@ -146,12 +146,12 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 
 func (pw *Visualizer) drawDefaultUI() {
 	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
-	x, y := pw.mousePos.X, pw.mousePos.Y
-	c := color.RGBA{R: 255, G: 255, B: 0, A: 1}
+	X, Y := pw.mousePos.X, pw.mousePos.Y
+	C := color.RGBA{R: 255, G: 255, B: 0, A: 1}
 
 	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
-	pw.w.Fill(image.Rect(x-150, y-100, x+150, y), c, draw.Src)
-	pw.w.Fill(image.Rect(x-50, y, x+50, y+100), c, draw.Src)
+	pw.w.Fill(image.Rect(X-150, Y-100, X+150, Y), C, draw.Src)
+	pw.w.Fill(image.Rect(X-50, Y, X+50, Y+100), C, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
